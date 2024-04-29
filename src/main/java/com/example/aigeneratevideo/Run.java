3eda7ed3b8c253a66dda9bd5e6e6b1c07a43e3cc
admin <admin@email.com>
2024-04-29 10:24:04 +0800

    修复解析json

--- src/main/java/com/example/aigeneratevideo/Run.java
+++ src/main/java/com/example/aigeneratevideo/Run.java
@@ -1,7 +1,5 @@
 package com.example.aigeneratevideo;
 
-import cn.hutool.core.io.FileUtil;
-import com.alibaba.fastjson.JSON;
 import com.example.aigeneratevideo.utils.FfmpegUtil;
 import com.example.aigeneratevideo.utils.StoryUtil;
 
--- src/main/java/com/example/aigeneratevideo/StoryService.java
+++ src/main/java/com/example/aigeneratevideo/StoryService.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.example.aigeneratevideo.utils.SecretKeyUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class StoryService {
@@ -30,6 +31,7 @@ public class StoryService {
         systemMessage.put("role", "system");
 
         systemMessage.put("content", "帮我生成一个小故事，里面有6个场景，以json形式返回，你需要返回故事的标题。" +
+                "你需要返回标准纯净的json，不要掺杂其它东西，因为我需要通过程序读取json。" +
                 "故事需要吸引人，引人入胜。你只需要模仿返回的格式，不需要模仿故事内容，故事你可以生成一个更好的。" +
                 "\n在每个场景里，你需要给出旁白朗读文字和图片生成提示词。" +
                 "\n旁白朗读文字，注意朗读文字不要很长。图片的提示词用于使用AI生成图片。" +
@@ -47,11 +49,15 @@ public class StoryService {
                 .bearerAuth(SecretKeyUtil.getSecretKey())
                 .body(getBody().toJSONString())
                 .execute().body();
-        String story = JSONObject.parseObject(response)
+
+        String storyJson = JSONObject.parseObject(response)
                 .getJSONArray("choices").getJSONObject(0)
                 .getJSONObject("message").getString("content");
-        log.info("生成得到的故事：" + story);
-        return JSONObject.parseObject(story, Story.class);
+        storyJson = StringUtils.removeStart(storyJson, "```json");
+        storyJson = StringUtils.removeEnd(storyJson, "```");
+
+        log.info("生成得到的故事：" + storyJson);
+        return JSONObject.parseObject(storyJson, Story.class);
     }
 
 }
